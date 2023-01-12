f47f49d9ad2376ec9696f6e5506650de0cfeca3e
diana.baughan <diana.baughan@bettercloud.com>
2023-01-12 18:45:00 -0500

    added conditional rendering of effect names to IngredientDisplay

--- __tests__/components/IngredientDisplay.test.tsx
+++ __tests__/components/IngredientDisplay.test.tsx
@@ -4,16 +4,31 @@ import Ingredients from "../../ingredients";
 import IngredientDisplay from "../../components/IngredientDisplay";
 
 describe("IngredientDisplay", () => {
-  let ingredients = Ingredients.assembleIngredientsPool(true, true, true);
-  it("should render the name of the ingredient", () => {
+  Ingredients.assembleIngredientsPool(true, true, true)
+  it("should render Chokeweed's name and its 4 effects", () => {
     render(
       <IngredientDisplay
-        ingredient={ingredients.find(
-          (ingredient) => ingredient.name === "Chokeweed"
-        )}
+        ingredient={Ingredients.findByName("Chokeweed")}
       />
     );
 
     expect(screen.getByText("Chokeweed")).toBeDefined();
+    expect(screen.getByText("Drain Luck")).toBeDefined();
+    expect(screen.getByText("Restore Fatigue")).toBeDefined();
+    expect(screen.getByText("Cure Common Disease")).toBeDefined();
+    expect(screen.getByText("Drain Willpower")).toBeDefined();
+
   });
+
+  it("should render Racer Plumes' name and its 2 effects", () => {
+    render(
+        <IngredientDisplay
+            ingredient={Ingredients.findByName("Racer Plumes")}
+        />
+    );
+
+    expect(screen.getByText("Racer Plumes")).toBeDefined();
+    expect(screen.getByText("Drain Willpower")).toBeDefined();
+    expect(screen.getByText("Levitate")).toBeDefined();
+  })
 });
--- components/IngredientDisplay/index.tsx
+++ components/IngredientDisplay/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Ingredient from "../../types/Ingredient";
+import Effect from "../../types/Effect"
 
 interface IngredientDisplayProps {
   ingredient: Ingredient | undefined;
@@ -8,7 +9,15 @@ interface IngredientDisplayProps {
 const IngredientDisplay: React.FC<IngredientDisplayProps> = ({
   ingredient,
 }) => {
-  return <h1>{ingredient ? ingredient.name : "nothing"}</h1>;
+  return (
+      <>
+        <h1>{ingredient ? ingredient.name : "nothing"}</h1>
+        <>
+            {ingredient?.effects.map(eff => <h2>{eff?.effectName}</h2>)}
+        </>
+
+      </>
+  );
 };
 
 export default IngredientDisplay;
